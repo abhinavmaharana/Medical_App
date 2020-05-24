85c286ec67043f00c7488e0aff688eb22090f447
abhinavmaharana <abhinavmaharana.004@gmail.com>
2020-05-24 12:40:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/medicalapp/detailscollectedactivity.java
@@ -0,0 +1,14 @@
+package com.example.medicalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailscollectedactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailscollectedactivity);
+    }
+}
--- app/src/main/java/com/example/medicalapp/docsactivity.java
+++ app/src/main/java/com/example/medicalapp/docsactivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class detailactivity extends AppCompatActivity {
+public class docsactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailactivity);
+        setContentView(R.layout.activity_docsactivity);
     }
 }
